2a0cc63e0f11513d0551ffb67cb7da0c66e52e9b
Nick Moore <nick@zoic.org>
2017-06-17 22:28:11 +1000

    slightly crazed telnet implementation w/o queues (needs locks?)

--- esp32/rrepl.c
+++ esp32/rrepl.c
@@ -8,59 +8,81 @@
 
 #include "esp_log.h"
 
-QueueHandle_t rrepl_tx_queue, rrepl_rx_queue;
-int rrepl_active = 0;
+int rrepl_fd = -1;
+int rrepl_rx_state = 0;
 
 void rrepl_init() {
-    rrepl_tx_queue = xQueueCreate(100,1);
-    rrepl_rx_queue = xQueueCreate(100,1);
 }
 
 void rrepl_task(void *pvParameter) {
     struct addrinfo *repl_ai;
     struct addrinfo hints = { .ai_family = AF_INET, .ai_socktype = SOCK_STREAM };
-    lwip_getaddrinfo("0.0.0.0", "2323", &hints, &repl_ai);
-    int repl_fd = lwip_socket(repl_ai->ai_family, repl_ai->ai_socktype, repl_ai->ai_protocol);
-    lwip_bind_r(repl_fd, repl_ai->ai_addr, repl_ai->ai_addrlen);
+    lwip_getaddrinfo("0.0.0.0", "23", &hints, &repl_ai);
+    int bound_fd = lwip_socket(repl_ai->ai_family, repl_ai->ai_socktype, repl_ai->ai_protocol);
+    lwip_bind_r(bound_fd, repl_ai->ai_addr, repl_ai->ai_addrlen);
     lwip_freeaddrinfo(repl_ai);
 
-    lwip_listen_r(repl_fd, 1);
+    lwip_listen_r(bound_fd, 1);
     struct sockaddr addr;
     socklen_t addr_len = sizeof(addr);
 
     for (;;) {
-        int fd = lwip_accept_r(repl_fd, &addr, &addr_len);
-	lwip_fcntl_r(fd, F_SETFL, O_NONBLOCK);
-        for (;;) {
-	    int c;
-	    if (xQueueReceive(rrepl_tx_queue, &c, 1) == pdTRUE) {
-		int r = lwip_write_r(fd, &c, 1);
-                if (r <= 0) break;
-	    }
-	    char buf[100];
-	    int r = lwip_read_r(fd, &buf, sizeof(buf));
-            if (r > 0) {
-	        for (int i=0; i<r; i++) {
-		   ESP_LOGI("rrepl_task", "RECV %d %02x", i, buf[i]);
-		   xQueueSend(rrepl_rx_queue, buf+i, 1);
-	        }
-	    }
-	    else if (r < 0 && errno != EWOULDBLOCK) { 
-		    ESP_LOGI("rrepl_task", "recvfrom failed! %d", errno); 
-		    break;
-	    }
-        }
-        ESP_LOGI("repl_task", "Close! %d", fd);
-        lwip_close_r(fd);
+        int fd = lwip_accept_r(bound_fd, &addr, &addr_len);
+	if (fd >= 0) {
+	    lwip_fcntl_r(fd, F_SETFL, O_NONBLOCK);
+	    if (rrepl_fd >= 0) lwip_close_r(rrepl_fd);
+	    rrepl_fd = fd;
+	    rrepl_rx_state = 0;
+	    lwip_write_r(rrepl_fd, "\xFF\xFB\x01\xFF\xFE\x01", 6); // TELNET IAC WILL ECHO IAC DON'T ECHO
+	}
     }
 }
 
 void rrepl_tx(char c) {
-    if (rrepl_active) xQueueSend(rrepl_tx_queue, &c, 0);
+    if (rrepl_fd >= 0) {
+        lwip_write_r(rrepl_fd, &c, 1);
+	if (c == 0xFF) lwip_write_r(rrepl_fd, &c, 1);
+    }
 }
 
+
 int rrepl_rx() {
-    if (rrepl_active) return -1;
     char c;
-    return xQueueReceive(rrepl_rx_queue, &c, 0) == pdTRUE ? c : -1;
+    if (rrepl_fd < 0) return -1;
+
+    for (;;) {
+        int r = lwip_read_r(rrepl_fd, &c, 1);
+        if (r < 0 && errno != EWOULDBLOCK) {
+            lwip_close_r(rrepl_fd);
+	    rrepl_fd = -1;
+	    return -1;
+        } 
+        if (r <= 0) return -1;
+
+        switch (rrepl_rx_state) {
+	    case 0:
+	        if (c != 0xFF) return c;
+	        rrepl_rx_state = 1;
+	        break;
+	    case 1:
+		switch (c) {
+		    case 0xFF: return c;
+                    case 0xFD: rrepl_rx_state = 2; break; // DO
+                    case 0xFE: rrepl_rx_state = 3; break; // DON'T
+                    case 0xFB: rrepl_rx_state = 3; break; // WILL
+                    case 0xFC: rrepl_rx_state = 3; break; // WON'T
+	            default: rrepl_rx_state = 0; break;   // DUNNO
+                }
+		break;
+	    case 2: 
+		if (c == 0x01) lwip_write_r(rrepl_fd, "\xFF\xFB\x01", 3); // WILL ECHO
+	        if (c == 0x03) lwip_write_r(rrepl_fd, "\xFF\xFB\x03", 3); // WILL SUPPRESS-GO-AHEAD
+		if (c == 0x05) lwip_write_r(rrepl_fd, "\xFF\xFC\x05", 3); // WON'T STATUS
+		// nobreak
+	    default:
+		rrepl_rx_state = 0;
+		break;
+	}
+    }
+    return -1;
 }
